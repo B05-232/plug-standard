7a6c01f43de7395770a06c69f03112e2ece126f7
Glebovski <andrewglebovski@gmail.com>
2023-11-15 23:00:48 +0300

    Implement Transform

--- src/Plug/Math/Transform.h
+++ src/Plug/Math/Transform.h
@@ -1,17 +1,68 @@
+#ifndef _TRANSFORM_H_
+#define _TRANSFORM_H_
 
 
-class Transform
-{
-    public:
+#include "Vec2d.h"
 
-    Vec2d getOffset () const;
-    Vec2d setOffset (const Vec2d &offset);
-    
-    Vec2d getScale  () const;
-    Vec2d setScale  (const Vec2d &scale);
 
-    Vec2d apply     (const Vec2d &vec);
-    Vec2d restore   (const Vec2d &vec);
+namespace plug {
+    class Transform {
+        public:
+            /**
+             * \brief Creates transform with the given offset and scale
+            */
+            Transform(const Vec2d &offset_ = Vec2d(), const Vec2d &scale_ = Vec2d(1, 1)) :
+                offset(offset_), scale(scale_) {}
 
-    Transform combine (const Transform &parent_transform);
-};
+            /**
+             * \brief Returns offset
+            */
+            Vec2d getOffset() const { return offset; }
+
+            /**
+             * \brief Sets offset
+            */
+            void setOffset(const Vec2d &offset_) { offset = offset_; }
+
+            /**
+             * \brief Returns scale
+            */
+            Vec2d getScale() const { return scale; }
+
+            /**
+             * \brief Sets scale
+            */
+            void setScale(const Vec2d &scale_) { scale = scale_; }
+
+            /**
+             * \brief Converts local position to on-screen position
+            */
+            Vec2d apply(const Vec2d &vec) const {
+                return offset + vec * scale;
+            }
+
+            /**
+             * \brief Converts on-screen position to local position
+            */
+            Vec2d restore(const Vec2d &vec) const {
+                return (vec - offset) / scale;
+            }
+
+            /**
+             * \brief Combines parent transform with this one
+            */
+            Transform combine(const Transform &parent_transform) {
+                return Transform(
+                    parent_transform.getOffset() + offset * parent_transform.getScale(),
+                    scale * parent_transform.getScale()
+                );
+            }
+        
+        private:
+            Vec2d offset;
+            Vec2d scale;
+    };
+}
+
+
+#endif
