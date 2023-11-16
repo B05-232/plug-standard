fba9e89005b7c82cd047465000c9cfb05efb537c
DenzeL2004 <work20022004@mail.ru>
2023-11-16 16:59:30 +0300

    add definition TransformStack.h

--- src/Plug/Math/TransformStack.h
+++ src/Plug/Math/TransformStack.h
@@ -1,13 +1,39 @@
-class TransformStack
+/**
+ * @file TransformStack.h
+ * @author DenzeL2004 (kolomiitsev.dv@phystech.edu)
+ *
+ * @brief
+ *
+ * @version 0.1
+ * @date 2023-11-16
+ *
+ * @copyright Copyright DenzeL2004 (c) 2023
+ */
+#ifndef _TRANSFORM_STACK_H_
+#define _TRANSFORM_STACK_H_
+
+#include "Vec2d.h"
+#include "Transform.h"
+
+namespace plug 
 {
-    public:
+    class TransformStack
+    {
+        public:
 
-    void enter (const Transform &transform);
-    void leave ();
-    
-    Transform top () const;
+        TransformStack(){}
 
-    Vec2d apply     (const Vec2d &vec) const; 
-    Vec2d restore   (const Vec2d &vec) const;
+        virtual ~TransformStack() = 0;
 
+        virtual void enter (const Transform &transform) = 0;
+        virtual void leave () = 0;
+        
+        virtual Transform top () const = 0;
+
+        virtual Vec2d apply     (const Vec2d &vec) const = 0; 
+        virtual Vec2d restore   (const Vec2d &vec) const = 0;
+
+    };
 };
+
+#endif
